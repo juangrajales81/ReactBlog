38f88baef075e05b74b3691482bfcdad42d83f5f
Juan Grajales <juangrajales81@gmail.com>
2021-01-04 18:28:25 -0500

    Initial commit

---
+++ initData.js
@@ -0,0 +1,12 @@
+[
+	{
+		"articleId":"987654",		
+		"articleTitle":"Article One",
+		"articleContent":"Here goes the content"
+	},
+	{
+		"articleId":"987655",		
+		"articleTitle":"Article Two",
+		"articleContent":"Sky is the limit, the content goes here"
+	}
+]
